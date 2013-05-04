1cb8660774f1a1036e537e4fad5ab28d8947da12
Ajunpreet Bambrah <ajunpret@gmail.com>
2013-05-04 13:22:17 -0400

    Basic Page setup for Rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 SampleApp::Application.routes.draw do
-  get "staticpages/home"
+  root :to => 'staticpages#home'
 
-  get "staticpages/help"
+  match 'signup', to: 'users#new'
+  match 'help', to: 'staticpages#help'
+  match 'about', to: 'staticpages#about'
+  match 'contact', to: 'staticpages#contact'
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
